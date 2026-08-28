b608e70d56ca4bbdf701eb5f995f3398d153f825
Bima Akbar <81931118+bimaakbar-dev@users.noreply.github.com>
2026-08-29 02:27:58 +0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://stargazers.pages.dev',
+	site: 'https://bimaakbar.pages.dev',
 	fonts: [
 		{
 			provider: fontProviders.local(),
