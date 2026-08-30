75db466340781fb635ed7748c2c1f5974f903cf6
Bima Akbar <81931118+bimaakbar-dev@users.noreply.github.com>
2026-08-30 23:30:28 +0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@
 import { defineConfig, fontProviders } from 'astro/config';
 import starlight from '@astrojs/starlight';
 import { sidebarConfig } from './src/config/sidebar';
+import { expressiveCodeConfig } from './src/config/sidebar';
 
 // https://astro.build/config
 export default defineConfig({
@@ -43,73 +44,6 @@ export default defineConfig({
 				light: './src/assets/images/light-logo.svg',
 				dark: './src/assets/images/dark-logo.svg',
 			},
-			favicon: '/images/favicon.svg',
-			customCss: [
-     		   './src/styles/reset.css',
-				'./src/styles/global.css',
-				'./src/styles/markdown.css',
-				'./src/styles/asides.css',
-			],
-			components: {
-				Banner: './src/components/Banner.astro',
-				Head: './src/components/Head.astro',
-				DraftContentNotice: './src/components/FallbackContentNotice.astro',
-				FallbackContentNotice: './src/components/FallbackContentNotice.astro',
-				
-			},
-			expressiveCode: {
-				styleOverrides: { 
-					borderColor: 'var(--sl-color-hairline)',
-					borderRadius: '0.5rem',
-					borderWidth: '1px',
-
-					codeBackground: 'var(--sl-color-gray-6)',
-					codeFontFamily: 'var(--font-geist-mono)',
-					codeFontSize: 'var(--sl-text-code-sm)',
-
-					focusBorder: 'var(--sl-color-accent)',
-
-					uiFontFamily: 'var(--font-geist)',
-
-					frames: {
-						copyIcon: 'var(--copy-icon)',
-
-						frameBoxShadowCssValue: 'none',
-						shadowColor: 'transparent',
-
-						editorActiveTabBackground: 'var(--sl-color-gray-6)',
-						editorActiveTabBorderColor: 'var(--sl-color-hairline)',
-						editorActiveTabIndicatorTopColor: 'none',
-						editorActiveTabForeground: 'var(--sl-color-text-accent)',
-						editorActiveTabIndicatorBottomColor: 'var(--sl-color-accent)',
-
-						editorBackground: 'var(--sl-color-gray-6)',
-						editorTabBarBackground: 'var(--sl-color-gray-6)',
-						editorTabBarBorderBottomColor: 'var(--sl-color-hairline)',
-						editorTabBarBorderColor: 'var(--sl-color-hairline)',
-						editorTabBorderRadius: '0.5rem',
-						
-						inlineButtonBackground: 'color-mix(in srgb, var(--sl-color-bg) 60%, transparent)',
-						inlineButtonBorder: 'var(--sl-color-hairline)',
-						inlineButtonForeground: 'var(--sl-color-white)',
-
-						terminalBackground: 'var(--sl-color-gray-6)',
-						terminalTitlebarBackground: 'var(--sl-color-gray-6)',
-						terminalTitlebarBorderBottomColor: 'var(--sl-color-hairline)',
-						terminalTitlebarDotsForeground: 'var(--sl-color-gray-2)',
-						terminalTitlebarDotsOpacity: '0.2',
-
-						tooltipSuccessBackground: 'var(--sl-color-green)',
-						tooltipSuccessForeground: 'var(--sl-color-text-invert)'
-					},
-				},
-				themes: [ 'vitesse-black', 'vitesse-light' ],
-				shiki: {
-					langAlias: {
-						markdoc: 'mdx',
-					},
-				},
-			},
 			defaultLocale: 'root',
 			locales: {
 				root: {
@@ -120,6 +54,21 @@ export default defineConfig({
 					label: 'Indonesia',
 					lang: 'id',
 				},
+			},
+			favicon: '/images/favicon.svg',
+			customCss: [
+     		   './src/styles/reset.css',
+				'./src/styles/global.css',
+				'./src/styles/markdown.css',
+				'./src/styles/asides.css',
+			],
+			expressiveCode: expressiveCodeConfig,
+			components: {
+				Banner: './src/components/Banner.astro',
+				Head: './src/components/Head.astro',
+				DraftContentNotice: './src/components/FallbackContentNotice.astro',
+				FallbackContentNotice: './src/components/FallbackContentNotice.astro',
+				
 			},
 			social: [
 				{ icon: 'github', label: 'GitHub', href: 'https://github.com/bimaakbar-dev/bimaakbar' }
