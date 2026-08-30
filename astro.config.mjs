e95881a42020060e3720c5b8ac4cfd235da01cbc
Bima Akbar <81931118+bimaakbar-dev@users.noreply.github.com>
2026-08-30 23:24:55 +0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 // @ts-check
 import { defineConfig, fontProviders } from 'astro/config';
 import starlight from '@astrojs/starlight';
+import { sidebarConfig } from './src/config/sidebar';
 
 // https://astro.build/config
 export default defineConfig({
@@ -50,7 +51,7 @@ export default defineConfig({
 				'./src/styles/asides.css',
 			],
 			components: {
-     Banner: './src/components/Banner.astro',
+				Banner: './src/components/Banner.astro',
 				Head: './src/components/Head.astro',
 				DraftContentNotice: './src/components/FallbackContentNotice.astro',
 				FallbackContentNotice: './src/components/FallbackContentNotice.astro',
@@ -123,43 +124,7 @@ export default defineConfig({
 			social: [
 				{ icon: 'github', label: 'GitHub', href: 'https://github.com/bimaakbar-dev/bimaakbar' }
 			],
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					items: [
-						{ autogenerate: { directory: 'reference' } }
-					],
-				},
-				{
-					label: 'Writing',
-					items: [
-						{ label: 'Markdown', slug: 'writing/markdown' },
-					],
-				},
-				{
-					label: 'Components',
-					items: [
-						{ label: 'Using Components', slug: 'components/using-components' },
-						{ label: 'Cards', slug: 'components/cards' },
-						{ label: 'Link Cards', slug: 'components/link-cards' },
-						{ label: 'Card Grids', slug: 'components/card-grids' },
-       				 { label: 'Asides', slug: 'components/asides' },
-            			{ label: 'Badges', slug: 'components/badges' },
-            			{ label: 'Code', slug: 'components/code' },
-            			{ label: 'File Tree', slug: 'components/file-tree' },
-            			{ label: 'Icons', slug: 'components/icons' },
-						{ label: 'Link Buttons', slug: 'components/link-buttons' },
-						{ label: 'Steps', slug: 'components/steps' },
-						{ label: 'Tabs', slug: 'components/tabs' },
-					],
-				},
-			],
+			sidebar: sidebarConfig,
 			credits: true,
 		}),
 	],
