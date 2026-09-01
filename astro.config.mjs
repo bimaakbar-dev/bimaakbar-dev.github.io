a51036fca3267c3f4ba014f3e0740f8d85465409
Bima Akbar <81931118+bimaakbar-dev@users.noreply.github.com>
2026-09-01 15:17:06 +0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -54,7 +54,7 @@ export default defineConfig({
 				FallbackContentNotice: './src/components/FallbackContentNotice.astro',
 				Head: './src/components/Head.astro',
      Hero: './src/components/Hero.astro',
-     MobileMenuToggle: './src/components/MobileMenuToggle.astro',
+     MarkdownContent: './src/components/MarkdownContent.astro',
      PageSidebar: './src/components/PageSidebar.astro',
 				PageTitle: './src/components/PageTitle.astro',
      Sidebar: './src/components/Sidebar.astro',
