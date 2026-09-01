24fb32d08eedcbb9774a9998eee36d911319aa9f
Bima Akbar <81931118+bimaakbar-dev@users.noreply.github.com>
2026-09-01 09:08:16 +0700

    Create navigation.ts

---
+++ src/config/navigation.ts
@@ -0,0 +1,9 @@
+export interface NavSection {
+  label: string;
+  href: string;
+}
+
+export const navSections: NavSection[] = [
+  { label: 'Docs', href: '/' },
+  { label: 'Blog', href: '/blog' },
+];
