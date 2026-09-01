503f588995c475f9c00acc3aa6975041fad0731b
Bima Akbar <81931118+bimaakbar-dev@users.noreply.github.com>
2026-09-01 17:34:18 +0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@
 import { defineConfig, fontProviders } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
-import { fontsConfig } from './src/config/fonts';
 import { sidebarConfig } from './src/config/sidebar';
 import { expressiveCodeConfig } from './src/config/expressive-code';
 
@@ -10,57 +9,95 @@ import { satteri } from '@astrojs/markdown-satteri';
 import { satteriExternalLink } from './src/lib/plugins/satteri/external-link';
 import { satteriTable } from './src/lib/plugins/satteri/table';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://bimaakbar-dev.github.io',
-	fonts: fontsConfig,
-	markdown: {
-    	processor: satteri({
-			hastPlugins: [
-				satteriExternalLink,
-				satteriTable
-			],
-    	}),
-	},
-	integrations: [
-		starlight({
-			title: 'stargazers',
-			defaultLocale: 'root',
-			locales: {
-				root: {
-					label: 'English',
-					lang: 'en',
-				},
-				id: {
-					label: 'Indonesia',
-					lang: 'id',
-				},
-			},
-			logo: {
-				light: './src/assets/images/light-logo.svg',
-				dark: './src/assets/images/dark-logo.svg',
-			},
-			favicon: '/images/favicon.svg',
-			customCss: [
-     		   './src/styles/reset.css',
-				'./src/styles/global.css',
-				'./src/styles/markdown.css',
-				'./src/styles/asides.css',
-			],
-			expressiveCode: expressiveCodeConfig,
-			components: {
-				Banner: './src/components/Banner.astro',
-				DraftContentNotice: './src/components/FallbackContentNotice.astro',
-				FallbackContentNotice: './src/components/FallbackContentNotice.astro',
-				Head: './src/components/Head.astro',
-				SiteTitle: './src/components/SiteTitle.astro',
-				
-			},
-			social: [
-				{ icon: 'github', label: 'GitHub', href: 'https://github.com/bimaakbar-dev/bimaakbar' }
-			],
-			sidebar: sidebarConfig,
-			credits: true,
-		}),
-	],
+    site: 'https://bimaakbar-dev.github.io',
+    fonts: [
+        {
+            provider: fontProviders.local(),
+            name: 'Geist',
+            cssVariable: '--font-geist',
+            options: {
+                variants: [
+                    {
+                        weight: '100 900',
+                        style: 'normal',
+                        src: ['./src/assets/fonts/Geist.woff2'],
+                    },
+                ],
+            },
+        },
+        {
+            provider: fontProviders.local(),
+            name: 'GeistMono',
+            cssVariable: '--font-geist-mono',
+            options: {
+                variants: [
+                    {
+                        weight: 'normal',
+                        style: 'normal',
+                        src: ['./src/assets/fonts/GeistMono.woff2'],
+                    },
+                ],
+            },
+        }
+    ],
+
+    markdown: {
+        processor: satteri({
+            hastPlugins: [
+                satteriExternalLink,
+                // @ts-ignore
+                satteriTable
+            ],
+        }),
+    },
+
+    integrations: [
+        starlight({
+            title: 'stargazers',
+            defaultLocale: 'root',
+            locales: {
+                root: {
+                    label: 'English',
+                    lang: 'en',
+                },
+                id: {
+                    label: 'Indonesia',
+                    lang: 'id',
+                },
+            },
+            logo: {
+                light: './src/assets/images/light-logo.svg',
+                dark: './src/assets/images/dark-logo.svg',
+            },
+            favicon: '/images/favicon.svg',
+            customCss: [
+                './src/styles/reset.css',
+                './src/styles/global.css',
+                './src/styles/markdown.css',
+                './src/styles/asides.css',
+            ],
+            // @ts-ignore
+            expressiveCode: expressiveCodeConfig,
+            components: {
+                Banner: './src/components/Banner.astro',
+                DraftContentNotice: './src/components/FallbackContentNotice.astro',
+                FallbackContentNotice: './src/components/FallbackContentNotice.astro',
+                Head: './src/components/Head.astro',
+                
+            },
+            social: [
+                { icon: 'github', label: 'GitHub', href: 'https://github.com/bimaakbar-dev/bimaakbar' }
+            ],
+            sidebar: sidebarConfig,
+            credits: true,
+        }),
+    ],
+
+    vite: {
+        plugins: [tailwindcss()],
+    },
 });
