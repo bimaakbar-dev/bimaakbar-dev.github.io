94771d09fd2750dfec252a924b3ab2e8009d9907
Bima Akbar <81931118+bimaakbar-dev@users.noreply.github.com>
2026-09-04 01:54:23 +0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -61,34 +61,6 @@ export default defineConfig({
                 ],
             },
         },
-        {
-            provider: fontProviders.local(),
-            name: 'Geist',
-            cssVariable: '--font-geist',
-            options: {
-                variants: [
-                    {
-                        weight: '100 900',
-                        style: 'normal',
-                        src: ['./src/assets/fonts/Geist.woff2'],
-                    },
-                ],
-            },
-        },
-        {
-            provider: fontProviders.local(),
-            name: 'GeistMono',
-            cssVariable: '--font-geist-mono',
-            options: {
-                variants: [
-                    {
-                        weight: 'normal',
-                        style: 'normal',
-                        src: ['./src/assets/fonts/GeistMono.woff2'],
-                    },
-                ],
-            },
-        },
       
     ],
     markdown: {
