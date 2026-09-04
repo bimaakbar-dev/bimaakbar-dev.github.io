9ac27aa3685c24c46182af82493caf895f75545c
Bima Akbar <81931118+bimaakbar-dev@users.noreply.github.com>
2026-09-04 12:55:18 +0700

    Update sidebar.ts

--- src/config/sidebar.ts
+++ src/config/sidebar.ts
@@ -3,16 +3,16 @@ export const sidebarConfig = [
   {
     label: 'Getting Started',
       items: [
-      { label: 'introduction', slug: 'getting-started/introduction' },
-    ],
+        { label: 'introduction', slug: 'getting-started/introduction' }
+      ],
   },
   {
     label: 'Writing',
-    items: [
-     { autogenerate: { directory: 'docs/writing' }, },
-     { label: 'Markdown', slug: 'docs/writing/markdown' },
-     { label: 'Using Mdx, slug: 'docs/writing/using-mdx' },
-    ],
+      items: [
+        { autogenerate: { directory: 'docs/writing' } },
+        { label: 'Markdown', slug: 'docs/writing/markdown' },
+        { label: 'Using MDX', slug: 'docs/writing/using-mdx' }
+      ],
   },
   {
     label: 'Components',
