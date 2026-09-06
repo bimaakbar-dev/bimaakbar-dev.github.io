06b59c572df3208284d74ff7d4d7ee1aad810318
Bima Akbar <81931118+bimaakbar-dev@users.noreply.github.com>
2026-09-06 21:33:15 +0700

    Add files via upload

--- src/config/expressive-code.ts
+++ src/config/expressive-code.ts
@@ -6,40 +6,41 @@ export const expressiveCode: StarlightExpressiveCodeOptions = {
     borderColor: "var(--sl-color-hairline-light)",
     borderRadius: "0.5rem",
     borderWidth: "1px",
-    
-    codeBackground: "var(--sl-color-bg-inline-code)",
+
+    codeBackground: "var(--sl-color-bg)",
     codeFontFamily: "var(--font-firaCode)",
     codeFontSize: "var(--sl-text-code-sm)",
-    
-    focusBorder: "var(--sl-color-accent)",
+
+    focusBorder: "var(--sl-color-accent-low)",
     uiFontFamily: "var(--font-inter)",
     frames: {
       copyIcon: "var(--copy-icon)",
       frameBoxShadowCssValue: "none",
       shadowColor: "transparent",
-      
-      editorActiveTabBackground: "var(--sl-color-bg-inline-code)",
-      editorActiveTabBorderColor: "var(--sl-color-hairline-light)",
+
+      editorBackground: "var(--sl-color-bg)",
+
+      editorTabBarBackground: "var(--sl-color-bg)",
+      editorTabBarBorderBottomColor: "var(--sl-color-hairline-light)",
+      editorTabBarBorderColor: "var(--sl-color-hairline-light)",
+      // editorTabBorderRadius: "0.5rem",
+
+      editorActiveTabBackground: "none",
+      editorActiveTabBorderColor: "none",
       editorActiveTabIndicatorTopColor: "none",
       editorActiveTabForeground: "var(--sl-color-white)",
       editorActiveTabIndicatorBottomColor: "var(--sl-color-accent-high)",
-      
-      editorBackground: "var(--sl-color-bg-inline-code)",
-      editorTabBarBackground: "var(--sl-color-bg)",
-      editorTabBarBorderBottomColor: "var(--sl-color-hairline)",
-      editorTabBarBorderColor: "var(--sl-color-hairline-light)",
-      editorTabBorderRadius: "0.5rem",
-      
-      inlineButtonBackground: "transparent",
-      inlineButtonBorder: "transparent",
+
+      inlineButtonBackground: "none",
+      inlineButtonBorder: "none",
       inlineButtonForeground: "var(--sl-color-white)",
-      
-      terminalBackground: "var(--sl-color-bg-inline-code)",
-      terminalTitlebarBackground: "var(--sl-color-bg)",
-      terminalTitlebarBorderBottomColor: "var(--sl-color-hairline)",
-      terminalTitlebarDotsForeground: "var(--sl-color-gray-3)",
+
+      terminalBackground: "var(--sl-color-bg)",
+      terminalTitlebarBackground: "none",
+      terminalTitlebarBorderBottomColor: "var(--sl-color-hairline-light)",
+      terminalTitlebarDotsForeground: "var(--sl-color-gray-4)",
       terminalTitlebarDotsOpacity: "0.3",
-      
+
       tooltipSuccessBackground: "var(--sl-color-green)",
       tooltipSuccessForeground: "var(--sl-color-text-invert)",
     },
