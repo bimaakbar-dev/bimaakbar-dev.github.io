eda665a861961cadaa323be1af374556e0eaf254
Bima Akbar <81931118+bimaakbar-dev@users.noreply.github.com>
2026-09-12 21:34:08 +0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -113,7 +113,7 @@ export default defineConfig({
       defaultLocale: "root",
       locales: siteLocale,
       expressiveCode: { 
-        // themes: ["github-dark", "vitesse-light"],
+        themes: ["github-dark", "vitesse-light"],
         shiki: {
           langs: [markdocGrammar] 
         }
