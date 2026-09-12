114958ab3e06074ef9e20ea5c9790b57dc8f9aac
Bima Akbar <81931118+bimaakbar-dev@users.noreply.github.com>
2026-09-12 19:09:57 +0700

    Add files via upload

---
+++ src/stradocs/components/index.ts
@@ -0,0 +1,7 @@
+// src/stradocs/components/index.ts
+
+export { default as CardGrid } from './CardGrid.astro';
+export { default as ContributorList } from './ContributorList.astro';
+export { default as GithubCard } from './GithubCard.astro';
+export { default as PreviewPanel } from './PreviewPanel.astro';
+export { default as Testimonial } from './Testimonial.astro';
