f0837da7119a5bf91388ccf869cf2e3a4ef653d9
Bima Akbar <81931118+bimaakbar-dev@users.noreply.github.com>
2026-09-12 18:33:26 +0700

    Delete src directory

--- src/config/navigation.ts
+++
@@ -1,9 +0,0 @@
-export interface NavSection {
-  label: string;
-  href: string;
-}
-
-export const navSections: NavSection[] = [
-  { label: "Docs", href: "/" },
-  { label: "Blog", href: "/blog" },
-];
--- src/constants.ts
+++
@@ -1,3 +0,0 @@
-const PAGE_TITLE_ID = "_top";
-
-export { PAGE_TITLE_ID };
