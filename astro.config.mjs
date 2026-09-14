1757f8468dae504a99ab43a0adbf96fd257412c9
Bima Akbar <81931118+bimaakbar-dev@users.noreply.github.com>
2026-09-14 12:13:43 +0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,15 @@
 // @ts-check
-import { defineConfig, fontProviders } from "astro/config";
+import { defineConfig, fontProviders, svgoOptimizer } from "astro/config";
 import starlight from "@astrojs/starlight";
 
-import markdocGrammar from './grammars/markdoc.tmLanguage.json'
-
 import { sidebarConfig } from "./src/config/sidebar";
+import markdocGrammar from './grammars/markdoc.tmLanguage.json'
 import { Overrides } from "./src/components/overrides";
 import { satteri } from "@astrojs/markdown-satteri";
 import { hastExternalLink } from "./src/lib/plugins/satteri/hast/hast-external-link";
 import { hastTable } from "./src/lib/plugins/satteri/hast/hast-table";
-import { mdastStradocsAside } from './src/lib/plugins/satteri/mdast/mdast-stradocs-aside';
+import { mdastStradocsAside } from './src/lib/plugins/satteri/mdast/mdast-stradocs-aside'
+import { mermaidPlugin } from "./src/lib/plugins/satteri/mdast/satteri-plugin-mermaid";
 
 const site = "https://bimaakbar-dev.github.io/";
 const siteName = "Stradocs";
@@ -76,19 +76,22 @@ export default defineConfig({
     processor: satteri({
       hastPlugins: [
         hastExternalLink,
-        hastTable,
+        hastTable
       ],
       mdastPlugins: [
         mdastStradocsAside,
+        mermaidPlugin,
       ],
       features: {
+        frontmatter: true,
         headingAttributes: true,
         directive: true,
+        definitionList: true,
         superscript: true,
         subscript: true,
         wikilinks: true,
-        definitionList: true,
         smartPunctuation: true,
+        math: { singleDollarTextMath: false },
         rawHtml: false
       },
     }),
@@ -104,11 +107,11 @@ export default defineConfig({
         replacesTitle: false,
       },
       favicon: "/images/favicon.svg",
-      customCss: ["./src/styles/global.css"],
       defaultLocale: "root",
       locales: siteLocale,
+      customCss: ["./src/styles/global.css"],
       expressiveCode: { 
-        themes: ["github-dark", "vitesse-light"],
+        themes: ["github-dark", "github-light"],
         shiki: {
           langs: [markdocGrammar] 
         }
@@ -129,11 +132,15 @@ export default defineConfig({
       lastUpdated: true,
       editLink: {
         baseUrl:
-          "https://github.com/bimaakbar-dev/bimaakbar-dev.github.io/edit/main/docs/docs/",
+          "https://github.com/bimaakbar-dev/bimaakbar-dev.github.io/edit/main/",
       },
       routeMiddleware: "./src/routeData.ts",
       credits: true,
-      sidebar: sidebarConfig,
+      sidebar: sidebarConfig
     }),
   ],
+  experimental: {
+    incrementalBuild: true,
+    svgOptimizer: svgoOptimizer(),
+  },
 });
