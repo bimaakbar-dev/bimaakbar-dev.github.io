0a11ec77623413737f9681cdfe9bbaf9d5cab603
Bima Akbar <81931118+bimaakbar-dev@users.noreply.github.com>
2026-09-18 23:08:40 +0700

    Delete src/stradocs/components directory

--- src/stradocs/components/index.ts
+++
@@ -1,7 +0,0 @@
-// src/stradocs/components/index.ts
-
-export { default as CardGrid } from './CardGrid.astro';
-export { default as ContributorList } from './ContributorList.astro';
-export { default as GithubCard } from './GithubCard.astro';
-export { default as PreviewPanel } from './PreviewPanel.astro';
-export { default as Testimonial } from './Testimonial.astro';
