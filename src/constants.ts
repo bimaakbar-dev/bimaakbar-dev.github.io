7a2ba05585ae080f1930c6d16eba49cbf349dce7
Bima Akbar <81931118+bimaakbar-dev@users.noreply.github.com>
2026-09-19 13:02:52 +0700

    Create constants.ts

---
+++ src/constants.ts
@@ -0,0 +1,25 @@
+// src/constanta.ts
+
+const PAGE_TITLE_ID = "_top";
+
+export { PAGE_TITLE_ID };
+
+
+export const SEO = {
+  verification: {
+    google: "ZxoHYWipAMoGhvwVi5nxolWsZOntrYkNkVcG9HTfFZQ",
+    bing: "",
+    yandex: "1878480c0d7b5510",
+  },
+
+  ads: {
+    google: "ca-pub-4943136052113535",
+    yandex: "R-A-19179231",
+    monetag: "98464dea950ab2405c90bcb6fc2f1226",
+  },
+
+  analytics: {
+    google: "G-2TKFP229HJ",
+    yandex: "108731154",
+  },
+} as const;
