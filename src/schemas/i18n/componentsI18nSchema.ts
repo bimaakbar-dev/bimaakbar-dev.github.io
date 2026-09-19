bbf375d91c55db289fec88ac00c294867234397e
Bima Akbar <81931118+bimaakbar-dev@users.noreply.github.com>
2026-09-19 13:01:40 +0700

    Delete src directory

--- src/schemas/i18n/componentsI18nSchema.ts
+++
@@ -1,8 +0,0 @@
-import { z } from 'astro/zod';
-
-export const componentsI18nSchema = () =>
-	z.object({
-		"component.preview": z.string().optional(),
-        "component.scrollToTop": z.string().optional(),
-	})
-.partial();
--- src/schemas/i18n/navigationI18nSchema.ts
+++
@@ -1,9 +0,0 @@
-import { z } from 'astro/zod';
-
-export const navigationI18nSchema = () =>
-	z.object({
-		"nav.blog": z.string().optional(),
-		"nav.docs": z.string().optional(),
-        "nav.home": z.string().optional(),
-	})
-.partial();
